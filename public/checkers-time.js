06b91cd6e0812a003cb21a35493f9be71a1cbab4
Osvaldo Del Valle, Jr <delvallejr.osvaldo84@yahoo.com>
2021-09-10 15:36:54 -0500

    checkers-mate-2

--- public/checkers-time.js
+++ public/checkers-time.js
@@ -1,16 +1,38 @@
+function setStyles(el, styleObj){
+    Object.keys(styleObj).map(key => el.setAttribute(key, styleObj[key]));
+}
 function CheckerBoard (props) {
-    return
+    let rows = []
+    for(let i = 0; i < props.size; i++){
+        rows.push(React.createElement(Row, {rows: props.size, key: i, even:(i%2===0)}));
+    }
+    return React.createElement("div", null, rows)
 }
 function Row (props) {
-    return
+    let cells = [];
+    let alt = props.even ? 0 : 1;
+    for(let i = alt; i < props.rows + alt; i++){
+        let cellType = (i%2===0) ? styles["colorA"] : styles["colorB"];
+        let cell = React.createElement(Cell, {cellStyle: cellType, key:i});
+        cells.push(cell);
+    };
+    let row = React.createElement("div", {style: styles["row"]}, cells);
+    return row;
 }
 function Cell (props) {
-    return
+    let styleProto = Object.create(styles["cell"]);
+    let obj = {
+        "backgroundColor": props.cellStyle["backgroundColor"]
+    };
+    Object.assign(obj, styleProto.__proto__)
+    return React.createElement("div", {style: obj}, "");
 }
 
 var styles = {
-    row: {height: '20px'},
-    cell: {height: '20px', width: '20px', display: 'inline-block'},
+    row: {height: '50px'},
+    cell: {height: '50px', width: '50px', display: 'inline-block'},
     colorA: {backgroundColor: 'black'},
     colorB: {backgroundColor: 'red'}
 }
+
+ReactDOM.render(React.createElement(CheckerBoard, {size:10}), document.getElementById("app"));
