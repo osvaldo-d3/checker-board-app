faf20bb2f926990c357df6fdcd86eb259a9f11ca
Osvaldo Del Valle, Jr <delvallejr.osvaldo84@yahoo.com>
2021-09-10 15:02:42 -0500

    checkers-mate

---
+++ public/checkers-time.js
@@ -0,0 +1,16 @@
+function CheckerBoard (props) {
+    return
+}
+function Row (props) {
+    return
+}
+function Cell (props) {
+    return
+}
+
+var styles = {
+    row: {height: '20px'},
+    cell: {height: '20px', width: '20px', display: 'inline-block'},
+    colorA: {backgroundColor: 'black'},
+    colorB: {backgroundColor: 'red'}
+}
